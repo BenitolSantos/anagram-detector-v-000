c40a6fe9833f91786223bc707015be2deec41f97
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-03 04:11:22 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,9 @@ class Anagram
     comparison_array.each do |i| #splitting per each word in the array.
       singular_word_array = i.split(//)
       if singular_word_array.sort == name_array.sort
-        matching_word << i
+        matching_word << i #if the singular word is sorted the same as the @name
+        #which had to first be split into an array
+        #push it to a seperate array and return it
       else
       end
     end
